44d53907820f85d3951fb32f383fad57e29b459c
Muhammad Hamza <hamzaicp54@gmail.com>
2023-02-27 19:36:30 +0500

    this has been updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,59 +1,11 @@
 const router = require("express").Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
+
+const authController = require("../controllers/authController");
+
 
 //REGISTER
-router.post("/register", async (req, res) => {
-  try {
-    //generate new password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    //create new user
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
-
-    const olduser = await User.findOne({ email: req.body.email });
-
-    if(!olduser){
-      //save user and respond
-      const user = await newUser.save();
-      res.status(200).json(user);
-
-    }
-    else
-    {
-      res.status(401).json({message:"This user is alredy exist !!"});
-    } 
-  }catch (err) {
-    res.status(500).json(err)
-  }
-});
-
+router.post("/register",authController.register);
 //LOGIN
-router.post("/login", async (req, res) => {
-  try {
-    const olduser = await User.findOne({ email: req.body.email });
-
-    if(!olduser)
-    {
-      res.status(404).json({message:"user not found"});
-    }
-    
-
-    const validPassword = await bcrypt.compare(req.body.password, olduser.password)
-
-    if(!validPassword){ 
-      
-      res.status(400).json({message:"wrong password"});
-   }
-   res.status(200).json(olduser)
-  }catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.post("/login",authController.login);
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,90 +1,19 @@
 const router = require("express").Router();
-const Post = require("../models/Post");
-const User = require("../models/User");
+const postController = require("../controllers/postController");
+
 
 //create a post
-
-router.post("/insert", async (req, res) => {
-  const newPost = new Post(req.body);
-  try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/insert", postController.create);
 //update a post
-
-router.put("/update/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
-    } else {
-      res.status(403).json("you can update only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put("/update/:id", postController.update);
 //delete a post
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.deleteOne();
-      res.status(200).json("the post has been deleted");
-    } else {
-      res.status(403).json("you can delete only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", postController.delete);
 //like / dislike a post
-
-router.put("/:id/like", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked");
-    } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put("/:id/like", postController.likes_dislikes);
 //get a post
-
-router.get("/getOne/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/getOne/:id", postController.getPost)
 
 //get timeline posts
-
-router.get("/timeline/all", async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.body.userId);
-    const userPosts = await Post.find({ userId: currentUser._id });
-    const friendPosts = await Promise.all(
-      currentUser.followings.map((friendId) => {
-        return Post.find({ userId: friendId });
-      })
-    );
-    res.json(userPosts.concat(...friendPosts))
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/timeline/all", postController.getTimeline);
 
 module.exports = router;
